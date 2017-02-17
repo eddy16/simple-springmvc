658b57f9d71e31c52eac08d6ed8cd019cc4c63ce
Edgar Cirilo <edcirilo_9216@live.com.mx>
2017-02-17 15:24:54 -0600

    Adding resources from template of materialize

---
+++ src/main/java/com/edcg/simple/springmvc/controller/IndexController.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.edcg.simple.springmvc.controller;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ *
+ * @author edgar
+ */
+@Controller
+@Scope("request")
+public class IndexController {
+    
+    @RequestMapping(value="/index",method=RequestMethod.GET)
+    public String index(){
+        return "index";
+    }
+    
+}
